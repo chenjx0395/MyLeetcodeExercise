8b35b1235815eb9c41544a21959bb4a1672347ba
chenjx0395 <150028230+chenjx0395@users.noreply.github.com>
2024-01-28 11:41:15 +0800

    E443. 压缩字符串【中等】E283. 移动零 【简单】 E392. 判断子序列【简单】E11. 盛最多水的容器【中等】

--- src/com/cjx/array/E283.java
+++ src/com/cjx/array/E283.java
@@ -7,7 +7,7 @@ package com.cjx.array;
  **/
 public class E283 {
 
-    public void moveZeroes(int[] nums) {
+    public void moveZeroes1(int[] nums) {
         int left = 0, right = 0;
         while (right < nums.length) {
             if (nums[right] != 0) {
@@ -21,6 +21,47 @@ public class E283 {
             right++;
         }
 
+    }
+
+    public void moveZeroes2(int[] nums) {
+        int length = nums.length;
+        if (length < 2) {
+            return;
+        }
+        int i = 0, j = 1;
+        while (j < length) {
+            if (nums[i] == 0 && nums[j] == 0) {
+                j++;
+            } else if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            } else {
+                i++;
+                j++;
+            }
+        }
+    }
+
+    /**
+     * 顶级思路
+     * @param nums
+     */
+    public void moveZeroes(int[] nums) {
+        int length = nums.length;
+        if (nums == null || length < 1){
+            return;
+        }
+        int j = 0;
+        for (int i = 0; i < length; i++) {
+            if (nums[i] != 0){
+                if (i > j){
+                    nums[j] = nums[i];
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
     }
 
 }
