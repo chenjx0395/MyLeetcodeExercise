5af8d15f5c30f595e1387690f8da4510edc0e249
chenjx0395 <150028230+chenjx0395@users.noreply.github.com>
2024-01-29 13:58:08 +0800

    E1679. K 和数对的最大数目【中等】 E643. 子数组最大平均数1【简单】 E1456. 定长子串中元音的最大数目【中等】E1004. 最大连续1的个数 III【中等】

---
+++ src/com/cjx/array/E1004.java
@@ -0,0 +1,38 @@
+package com.cjx.array;
+
+/**
+ * @Description E1004. 最大连续1的个数 III【中等】
+ * @Author cjx
+ * @Date 2024/1/29 13:14
+ **/
+public class E1004 {
+    public int longestOnes(int[] nums, int k) {
+        int length = nums.length;
+        int max = 0;
+        int left = 0 , right = 0;
+        while (right < length){
+            if (nums[right] == 0){
+                if (k < 1){
+                    while (left <= right){
+                        if (nums[left] == 0){
+                            left++;
+                            k++;
+                            break;
+                        }
+                        left++;
+                    }
+                }
+                k--;
+            }
+            right++;
+            max = Math.max(max, right - left);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new E1004().longestOnes(new int[]{0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1},3));
+
+    }
+
+}
