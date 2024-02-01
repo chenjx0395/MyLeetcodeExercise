3189f9b8dcf41b94fea3ebce43eb6fcae5de0fb1
chenjx0395 <150028230+chenjx0395@users.noreply.github.com>
2024-02-01 13:46:00 +0800

    栈：E394 队列：E933；E649

---
+++ src/com/cjx/queue/E649.java
@@ -0,0 +1,94 @@
+package com.cjx.queue;
+
+import javax.xml.stream.FactoryConfigurationError;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Optional;
+
+/**
+ * @Description E649. Dota2 参议院
+ * @Author cjx
+ * @Date 2024/2/1 12:08
+ **/
+public class E649 {
+    public String predictPartyVictory1(String senate) {
+        int rSize = 0, dSize = 0;
+        char[] charArray = senate.toCharArray();
+        for (char c : charArray) {
+            if (c == 'R') {
+                rSize++;
+            } else {
+                dSize++;
+            }
+        }
+        LinkedList<Character> queue = new LinkedList<>();
+        for (char c : charArray) {
+            queue.addLast(c);
+        }
+        int i = 0;
+        while (rSize != 0 && dSize != 0) {
+            if (i >= queue.size()) {
+                i = 0;
+            }
+            if (queue.get(i++) == 'R') {
+                //删除逻辑 ： 先判断其后有没有其他参议员，要删第一个，没有删全局第一个
+
+                queue.removeFirstOccurrence('D');
+                dSize--;
+            } else {
+                queue.removeFirstOccurrence('R');
+                rSize--;
+            }
+        }
+        return rSize > dSize ? "Radiant" : "Dire";
+
+    }
+
+    public String predictPartyVictory(String senate) {
+        char[] charArray = senate.toCharArray();
+        int length = charArray.length;
+        boolean flagR = true, flagD = true;
+        int flag = 0;
+        while (flagR && flagD) {
+            flagR = false;
+            flagD = false;
+            for (int i = 0; i < length; i++) {
+                char c = charArray[i];
+                if (c == 'R') {
+                    if (flag < 0) {
+                        charArray[i] = 0;
+                    } else {
+                        flagR = true;
+                    }
+                    flag++;
+
+
+                }
+                if (c == 'D') {
+                    if (flag > 0) {
+                        charArray[i] = 0;
+                    } else {
+                        flagD = true;
+                    }
+                    flag--;
+                }
+            }
+        }
+        return flagR ? "Radiant" : "Dire";
+    }
+
+    public boolean isCYY(LinkedList<?> queue, int first, int last) {
+        Object[] array = queue.toArray();
+        System.out.println(Arrays.toString(array));
+        return true;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(new E649().predictPartyVictory("DRRD"));
+        LinkedList<Character> objects = new LinkedList<>();
+        objects.add('a');
+        objects.add('b');
+        objects.add('c');
+        new E649().isCYY(objects, 0, 0);
+    }
+}
---
+++ src/com/cjx/stack/E394.java
@@ -0,0 +1,59 @@
+package com.cjx.stack;
+
+import java.util.*;
+
+/**
+ * @Description E394. 字符串解码【中等】
+ * @Author cjx
+ * @Date 2024/2/1 9:35
+ **/
+public class E394 {
+    public String decodeString(String s) {
+        int length = s.length();
+        Stack<Character> numStack = new Stack<>();
+        Stack<Character> charStack = new Stack<>();
+        StringBuilder res = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == ']') {
+                int num = 0;
+                if (!numStack.isEmpty()){
+                    numStack.pop();
+                }
+                //取数字
+                for (int j = 1; !numStack.isEmpty() && numStack.peek() != '['; j *= 10) {
+                    num += (numStack.pop() - 48) * j;
+                }
+                //取字符
+                StringBuilder tempBuilder = new StringBuilder();
+                for (int i = 0; charStack.peek() != '['; i++) {
+                    tempBuilder.append(charStack.pop());
+                }
+                tempBuilder.reverse();
+                String tempStr = tempBuilder.toString().repeat(num);
+                charStack.pop();
+
+                    for (char c1 : tempStr.toCharArray()) {
+                        charStack.push(c1);
+                    }
+
+            } else {
+                if (c >= 48 && c <= 57) {
+                    numStack.push(c);
+                } else if (c == '[') {
+                    numStack.push(c);
+                    charStack.push(c);
+                } else {
+                    charStack.push(c);
+                }
+            }
+        }
+        while (!charStack.isEmpty()) {
+            res.append(charStack.pop());
+        }
+        return res.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new E394().decodeString("3[a]2[bc]"));
+    }
+}
