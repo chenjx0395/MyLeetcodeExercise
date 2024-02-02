c2118693776c1d8cefb973a380b4946d37913666
chenjx0395 <150028230+chenjx0395@users.noreply.github.com>
2024-02-02 14:04:34 +0800

    链表：E2095；E328；E206

---
+++ src/com/cjx/linked_lists/E2095.java
@@ -0,0 +1,45 @@
+package com.cjx.linked_lists;
+
+import java.util.LinkedList;
+
+/**
+ * @Description E2095. 删除链表的中间节点【中等】
+ * @Author cjx
+ * @Date 2024/2/2 9:27
+ **/
+public class E2095 {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head.next == null){
+            return null;
+        }
+        if (head.next.next == null){
+            head.next = null;
+            return head;
+        }
+        ListNode sentryNode = new ListNode(0,head);
+        ListNode left = sentryNode , right = head;
+        while (right != null && right.next != null){
+            right = right.next.next;
+            left = left.next;
+        }
+        left.next = left.next.next;
+        return head;
+    }
+
+    private int getMiddle(ListNode head) {
+        int count = 1;
+        while (head.next != null) {
+            head = head.next;
+            count++;
+        }
+        return count / 2;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4))));
+        System.out.println(new E2095().deleteMiddle(head));
+
+    }
+
+}
+
