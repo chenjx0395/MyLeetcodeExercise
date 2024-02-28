35dd888bdab399135573d9c70e08ab9e93764c34
chenjx0395 <150028230+chenjx0395@users.noreply.github.com>
2024-02-28 17:16:15 +0800

    二叉树：E236；E437；E450；E700；E1372

---
+++ src/com/cjx/binarytree/E1372.java
@@ -0,0 +1,29 @@
+package com.cjx.binarytree;
+
+/**
+ * @author cjx
+ * @Description E1372. 二叉树中的最长交错路径【中等】
+ * @date 2024/2/14 10:59
+ **/
+public class E1372 {
+    public int longestZigZag(TreeNode root) {
+        int lRes = recursion(root.left,true,false);
+        int rRes = recursion(root.right,false,true);
+        return Math.max(lRes, rRes);
+
+    }
+
+    public int recursion(TreeNode node, boolean fatherSex, boolean childSex) {
+        if (node == null) {
+            return 0;
+        }
+        int res = 0;
+        if (fatherSex != childSex) {
+            res++;
+        }
+        int lRes = recursion(node.left, childSex, false);
+        int rRes = recursion(node.right, childSex, true);
+        res += Math.max(lRes, rRes);
+        return res;
+    }
+}
