b66dc06e6a5679a7bba47c3d4e2ee677515f3476
chenjx0395 <150028230+chenjx0395@users.noreply.github.com>
2024-03-03 11:24:22 +0800

    图深度优先遍历：E841

---
+++ src/com/cjx/graph/deep/E841.java
@@ -0,0 +1,42 @@
+package com.cjx.graph.deep;
+
+import com.sun.source.tree.NewArrayTree;
+
+import java.util.*;
+
+/**
+ * @author cjx
+ * @Description E841. 钥匙和房间【中等】
+ * @date 2024/3/3 10:43
+ **/
+public class E841 {
+    boolean[] vis;
+    int num;
+
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int n = rooms.size();
+        num = 0;
+        vis = new boolean[n];
+        dfs(rooms, 0);
+        return num == n;
+    }
+
+    public void dfs(List<List<Integer>> rooms, int x) {
+        vis[x] = true;
+        num++;
+        for (int it : rooms.get(x)) {
+            if (!vis[it]) {
+                dfs(rooms, it);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        List<List<Integer>> rooms = new ArrayList<>();
+        rooms.add(Arrays.asList(1, 3));
+        rooms.add(Arrays.asList(3, 0, 1));
+        rooms.add(Arrays.asList(2));
+        rooms.add(Arrays.asList(0));
+        new E841().canVisitAllRooms(rooms);
+    }
+
+}
